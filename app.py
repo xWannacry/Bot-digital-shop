7889dbc14aac7af8bf5f1b7774e0569b52e8a559
xWannacry <101018825+xWannacry@users.noreply.github.com>
2023-06-22 09:08:41 +0000

    fixed page routing

--- app.py
+++ app.py
@@ -4,8 +4,45 @@ app = Flask(__name__)
 
 
 @app.route('/')
+@app.route('/home')
+@app.route('/index')
 def Home():
   return render_template("index.html")
+
+
+@app.route('/login')
+def login():
+  return render_template("login.html")
+
+
+@app.route('/signup')
+def signup():
+  return render_template("signup.html")
+
+
+@app.route('/pricing')
+def pricing():
+  return render_template("pricing.html")
+
+
+@app.route('/services')
+def services():
+  return render_template("services.html")
+
+
+@app.route('/shopping-cart')
+def shopping_cart():
+  return render_template("shopping-cart.html")
+
+
+@app.route('/products')
+def products():
+  return render_template("products.html")
+
+
+@app.route('/product')
+def product():
+  return render_template("product.html")
 
 
 if __name__ == '__main__':
